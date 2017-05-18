6921fe59c614d9b703957f27a3a7bd1b746555f7
chris <bertinato@ldeo.columbia.edu>
2017-05-18 11:14:31 -0400

    Added tests for pandas Series input.

--- tests/longmantide_tests.py
+++ tests/longmantide_tests.py
@@ -2,6 +2,7 @@ from nose.tools import *
 from datetime import datetime
 import numpy as np
 from longmantide import longmantide
+import pandas as pd
 
 
 def test_basic():
@@ -14,3 +15,17 @@ def test_basic():
     np.testing.assert_almost_equal(gm, 0.0324029651226, 8)
     np.testing.assert_almost_equal(gs, -0.0288682178454, 8)
     np.testing.assert_almost_equal(g, 0.00353474727722, 8)
+
+def test_series():
+    lat = pd.Series(data=np.full(5, 40.7914))
+    lon = pd.Series(data=np.full(5, 282.1414))
+    alt = pd.Series(data=np.full(5, 370.))
+    model = longmantide.TideModel()
+    time = pd.DatetimeIndex(data=[datetime(2015, 4, 23, 0, 0, 0)]*5)
+    gm, gs, g = model.solve_longman(lat, lon, alt, time)
+    gm_expect = np.full(5, 0.0324029651226)
+    gs_expect = np.full(-0.0288682178454)
+    g_expect = np.full(0.00353474727722)
+    np.testing.assert_array_almost_equal(gm, gm_expect, 8)
+    np.testing.assert_array_almost_equal(gs, gs_expect, 8)
+    np.testing.assert_array_almost_equal(g, g_expect, 8)
